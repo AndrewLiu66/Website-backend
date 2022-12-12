a5924f84ff8be25f5d919e876bb40d47a0777ff0
AndrewLiu66 <zhaoyu.liu@hotmail.com>
2022-12-11 23:42:09 -0800

    change build

---
+++ build/static/js/200.93bddb72.chunk.js
@@ -0,0 +1,2 @@
+/*! For license information please see 200.93bddb72.chunk.js.LICENSE.txt */
+(this["webpackJsonpmatx-react-pro"]=this["webpackJsonpmatx-react-pro"]||[]).push([[200],{1349:function(t,n,e){"use strict";e.r(n),e.d(n,"CalciteAction",(function(){return c})),e.d(n,"defineCustomElement",(function(){return a}));var r=e(919),c=r.a,a=r.b}}]);
